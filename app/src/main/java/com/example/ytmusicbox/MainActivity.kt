1b4debc43522e7272615e9fa425a2d2c4b8c69e8
Sean McGlothlin <sean.michael.mcglothlin@gmail.com>
2020-08-21 12:35:21 +0100

    init project

---
+++ app/src/main/java/com/example/ytmusicbox/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.ytmusicbox
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.app.DownloadManager
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
